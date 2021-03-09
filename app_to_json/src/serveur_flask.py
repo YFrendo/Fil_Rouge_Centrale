01aca1ba714155463b36c151f5d970ab5e8cd1a7
YFrendo <yann.frendo@live.fr>
2021-03-09 15:51:36 +0100

    Update serveur_flask.py

--- app_to_json/src/serveur_flask.py
+++ app_to_json/src/serveur_flask.py
@@ -13,11 +13,11 @@ import boto3
 app = Flask(__name__)
 
 
-UPLOAD_FOLDER = './static/uploads/'
+UPLOAD_FOLDER = './static/uploads/' #Variable qui permet de gérer ou est le fichier de stockage temporaire
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/static/<path:path>')
+@app.route('/static/<path:path>') #Tout ce qui est en dessous permet de gérer le swagger
 def send_static(path):
     return send_from_directory('static',path)
 
@@ -32,60 +32,60 @@ swaggerui_blueprint = get_swaggerui_blueprint(
         )
 app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
-@app.route('/upload', methods = ['GET','POST'])
+@app.route('/upload', methods = ['POST']) #Route /upload qui permet de upload un ficheir et d'avoir la transformation JSON
 def to_json():
 
-    if not os.path.isdir(app.config['UPLOAD_FOLDER']):
+    if not os.path.isdir(app.config['UPLOAD_FOLDER']): #On vérifie l'existance du fichier Upload
             os.makedirs(app.config['UPLOAD_FOLDER'])
     if request.method == 'POST':
-        if 'file' not in request.files:
+        if 'file' not in request.files: #Si il n'y a pas de file dans la requette on renvoie une erreur
             return 'No file part',400
-        upload_file = request.files['file']
+        upload_file = request.files['file'] #Sinon on la télécharge
 
-        if upload_file.filename == '':
+        if upload_file.filename == '': #Si il y a une file mais rien dedans on renvoie une erreur
             return 'No selected file',400
-        _, ext = os.path.splitext(upload_file.filename)
+        _, ext = os.path.splitext(upload_file.filename) #On extrait le MIME du fichier qui va nous permettre de savoir de quel type il est
        
-        upload_file.save(os.path.join(app.config['UPLOAD_FOLDER'],'temporary_file')) #Enregistre de facon temporaire
+        upload_file.save(os.path.join(app.config['UPLOAD_FOLDER'],'temporary_file')) #Enregistre de facon temporaire le fichier
         path = app.config['UPLOAD_FOLDER'] + 'temporary_file'
 
-        if ext == ".txt":
+        if ext == ".txt": #Si le fichier est un fichier texte
             sortie = txt_json(path)
-            if isinstance(sortie,str):
+            if isinstance(sortie,str): #Si la sortie est un str alors il y a eu une erreur et on renvoie le message d'erreur
                 return(sortie,400)
             sortie_json, metadata = sortie
-            metadata["Nom fichier"] = upload_file.filename
-        elif ext == ".pdf":
+            metadata["Nom fichier"] = upload_file.filename #On rajoute le nom du fichier
+        elif ext == ".pdf": #Idem pour PDF
             sortie = pdf_json(path)
             if isinstance(sortie,str):
                 return(sortie,400)
             sortie_json, metadata = sortie
             metadata["Nom fichier"] = upload_file.filename
-        elif ext == ".csv":
+        elif ext == ".csv": #Idem pour CSV
             sortie = csv_json(path)
             if isinstance(sortie,str):
                 return(sortie,400)
             sortie_json, metadata = sortie
             metadata["Nom fichier"] = upload_file.filename
-        else:
+        else: #Si ce n'est rien de tout ca alors on teste si c'est une image
             try:
                 sortie_json,metadata = image_json(path)
-            except:
-                return("Extension non supporté ou non existante",400)
+            except: #Si cella ne marche pas on renvoie une erreur
+                return("Extension non supporté,non existante ou incorrecte",400)
 
-        os.remove('./static/uploads/temporary_file') #On enlève le fichier
-        dico_sortie = {'Transfo_json':sortie_json,'Metadata':metadata}
+        os.remove('./static/uploads/temporary_file') #On enlève le fichier qui n'est plus utile
+        dico_sortie = {'Transfo_json':sortie_json,'Metadata':metadata} #On créer le dico de sortie
 
         s3 = boto3.client('s3')
-        id_dc = str(uuid.uuid4())
+        id_dc = str(uuid.uuid4())#On va créer une ID pseudo unique pour le fichier (10^38 ID possible)
 
-        dico_sortie['ID'] = id_dc
-        with open("./static/" + str(id_dc) + ".json", "w") as file:
+        dico_sortie['ID'] = id_dc #On rajoute l'ID au JSON de sortie
+        with open("./static/" + str(id_dc) + ".json", "w") as file: #On créer un JSON avec la sortie 
                 json.dump(dico_sortie, file)
         with open('./static/' + id_dc +'.json', 'rb') as f:
-            val = s3.upload_fileobj(f, "filrouge",id_dc)
+            val = s3.upload_fileobj(f, "filrouge",id_dc) #On upload le fichier sur le bucketS3 AWS
         if val == False:
-            id_dc = 's3fail'
+            id_dc = 's3fail' #Si il y a un problème avec S3
         
 
         @after_this_request #Après envoie on vide les fichiers temporaire
@@ -94,28 +94,28 @@ def to_json():
             return(response)
         
 
-        response = make_response( send_from_directory(directory = './static/', filename = id_dc + '.json'))
-        #response.headers['content-disposition'] = "attachment; filename=rendu.zip"
-        return response, 200
+        response = make_response( send_from_directory(directory = './static/', filename = id_dc + '.json')) #On fabrique la réponse
+        #response.headers['content-disposition'] = "attachment; filename=rendu.zip" Permet de télécharger via swagger
+        return response, 200 #Et on envoie
 
 
 @app.route('/stockage/<id_dos>', methods = ['GET'])
-def stockage(id_dos):
+def stockage(id_dos): #Permet de récupérer un JSON enregistré dans S3
     s3 = boto3.client('s3')
     try:
-        s3.download_file("filrouge",str(id_dos),'./static/' + str(id_dos) + '.zip')
+        s3.download_file("filrouge",str(id_dos),'./static/' + str(id_dos) + '.json') #On télécharge sur S3
     except:
         return 'No file at this ID',404
 
     @after_this_request
-    def remove_demande(response):
-        os.remove('./static/' + str(id_dos) + '.zip')
+    def remove_demande(response): #On supprime fichier temporaire
+        os.remove('./static/' + str(id_dos) + '.json')
         return (response)
 
-    response = make_response( send_from_directory(directory = './static/', filename = id_dos + '.zip'))
-    response.headers['content-disposition'] = "attachment; filename=rendu.zip"
+    response = make_response( send_from_directory(directory = './static/', filename = id_dos + '.json')) #On renvoie
+    #response.headers['content-disposition'] = "attachment; filename=rendu.json" Permet de télécharger via swagger
     return response, 200
 
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=5000) 
+    app.run(debug=False, host='0.0.0.0', port=5000) #Démarage de Flask
